d546b838b075bed6c8c0c1a9467159fdf91f35bb
camillatofani <camillatofani@gmail.com>
2023-11-02 18:26:01 +0100

    continued to implement the game logic

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,12 @@ function App() {
 	const [playerOne, setPlayerOne] = useState(false)
 	const [playerTwo, setPlayerTwo] = useState(false)
 	const [inputNamePlayer, setInputNamePlayer] = useState('')
+	const [played, setPlayed] = useState([])
 
-	function getMoveId(e) {
-		console.log(e.target.dataset.id)
-	}
-
+	// Logic for the player name
 	function setNamePlayer(e) {
 		setInputNamePlayer(e.target.value)
 	}
-
 	function confirmNamePlayer() {
 		if (!playerOne) {
 			setPlayerOne(inputNamePlayer)
@@ -22,17 +19,42 @@ function App() {
 			setPlayerTwo(inputNamePlayer)
 		}
 	}
-
 	function valueAction(e) {
 		if (e.keyCode === 13) {
 			confirmNamePlayer()
 		}
 	}
 
+	// Logic for the game
+	function getMoveId(e) {
+		var playedNow = e.target.dataset.id
+		setPlayed(prev => [...prev, playedNow])
+		const verify = [...played, playedNow]
+		whoWin(verify)
+	}
+	function whoWin(verify) {
+		if (verify.length === 2) {
+			couples.forEach(coup => {
+				if (verify[0] === coup[0]) {
+					if (verify[1] === coup[1]) {
+						console.log('you win')
+					}
+				}
+			})
+			console.log('verify', verify)
+			setPlayed([])
+		}
+	}
+
+	// 0 = equals, 1 = lose, 2 = win (playerOne)
+	const couples = [
+		['0', '2'],
+	]
+
 	if (!playerOne || !playerTwo) {
 		return (
 			<div className='game'>
-				<Title title={ 'Rock, paper or scissor?' } tag={ 'h1' } />
+				<Title title={ 'Rock , paper or scissor?' } tag={ 'h1' } />
 				Before start, please, enter the name of { !playerOne ? 'first' : 'second' } player
 				<br />
 				<input placeholder='Name' value={ inputNamePlayer } onChange={ setNamePlayer } onKeyDown={ valueAction }></input>
@@ -47,14 +69,23 @@ function App() {
 		<div className='game'>
 			<Title title={ 'Rock, paper or scissor?' } tag={ 'h1' } />
 			<div className='info'>
-				<div>player { playerOne }</div>
-				<div>player { playerTwo }</div>
+				<div>
+					<div className='player'>player one</div>
+					<div className='playerName'>{ playerOne }</div>
+				</div>
+				<div>
+					<div className='player'>player two</div>
+					<div className='playerName'>{ playerTwo }</div>
+				</div>
 			</div>
 			<div className='box-moves'>
 				<div data-id="0" onClick={ getMoveId } className='move'>👊🏻</div>
 				<div data-id="1" onClick={ getMoveId } className='move'>🤚🏻</div>
 				<div data-id="2" onClick={ getMoveId } className='move'>✌🏻</div>
 			</div>
+			{ couples.map((single, index) => (
+				<p key={ index }>one: { single.one }, two: { single.two }, win: { single.win }</p>
+			)) }
 		</div>
 	)
 }
